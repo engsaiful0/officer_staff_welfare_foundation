f3f519b7ad68cc51a121d2d50731d8deb060b1ee
sislam <sislam@storygeny.com>
2025-12-28 07:40:39 +0600

    menu edit

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -9,8 +9,8 @@ $configData = Helper::appClasses();
   @if(!isset($navbarFull))
     <div class="app-brand demo">
       <a href="{{url('/')}}" class="app-brand-link">
-        <span class="app-brand-logo demo">@include('_partials.macros',["height"=>20])</span>
-        <span class="app-brand-text demo menu-text fw-bold">{{config('variables.templateName')}}</span>
+        <span class="app-brand-logo demo"><img style="height: 30px;width: 30px;" src="{{ asset('assets/img/branding/'.$appSettings->logo) }}" alt="Logo"></span>
+        <span class="app-brand-text demo menu-text fw-bold">{{ $appSettings->app_name ?? config('app.name') }}</span>
       </a>
 
       <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
